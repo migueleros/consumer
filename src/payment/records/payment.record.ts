dda98a958fab509a9644838f6b90e222bf72b388
Henrique Miranda Paulino <henriquepaulino-ieg@instituto.ad>
2024-05-17 09:38:21 -0300

    I forgo to push everything

--- src/payment/records/payment.record.ts
+++ src/payment/records/payment.record.ts
@@ -1,8 +1,14 @@
 import { Column, Entity } from "typeorm";
 import { IPayment } from "../interfaces/payment.interface";
+import { paymentMethods } from "../enum/payment.enum";
+import { IProduct } from "../interfaces/product.interface";
 
 @Entity()
 export class Payment implements IPayment {
     @Column()
-    cpf: string;
+    idClient: number;
+    @Column()
+    paymentMethod: paymentMethods;
+    @Column()
+    product: IProduct[];
 }
